0534861ae7951f4ed52bdd1814830fdeb63c23da
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-03 14:49:39 +0300

    Add calculator functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
-var add = function(number1, number2) {
-	return number1 + number2;
-};
+const myHeading = document.querySelector('h1').textContent = "Calculator";
 var number1 = parseInt(prompt("Enter a number:"));
+var calculateSign = prompt("Enter a calculation operator/sign");
 var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
+document.getElementById('no1').textContent = number1;
+document.getElementById('no2').textContent = calculateSign;
+document.getElementById('no3').textContent = number2;
+document.getElementById('no4').textContent = calculate();
+
+function calculate() {
+  if (calculateSign == '+') {
+    let result = number1 + number2;
+    return result;
+  }else if (calculateSign == '-') {
+    let result = number1 - number2;
+    return result;
+  }else if (calculateSign == '*') {
+    let result = number1 * number2;
+    return result;
+  }else if (calculateSign == '/') {
+    let result = number1 / number2;
+    return result;
+  }else if (calculateSign == '%') {
+    let result = number1 % number2;
+    return result;
+  }else {
+    return "Use correct Inputs";
+  }
+}
+calculate();
